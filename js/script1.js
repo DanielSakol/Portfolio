b9e6c20c487311db4899a9a388ec1d74297b4f5b
Danny Acne <dannyacne@gmail.com>
2019-05-25 11:59:37 -0500

    drop menu issue

--- js/script1.js
+++ js/script1.js
@@ -1,1 +1,18 @@
-console.log("yooo")
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+document.getElementById("dropper").onclick = function() {myFunction()};
+
+function myFunction() {
+console.log("clicked")
+  document.getElementById("dropper").classList.toggle("show");
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
